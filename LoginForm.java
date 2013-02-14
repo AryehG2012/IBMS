2efe379939a16f4e40a3f71675249ed1c55673c6
Aryeh Grosskopf <grosska1@cs.man.ac.uk>
2013-02-14 19:24:25 +0000

    I'm adding the LoginForm class

---
+++ LoginForm.java
@@ -0,0 +1,15 @@
+package IBMS.Authentication
+
+// GUI Basics
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LoginForm
+{
+  public LoginForm
+  {
+    
+  } // LoginForm constructor
+} // LoginForm class
